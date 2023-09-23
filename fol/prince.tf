f437932507aee12d2853ba8ab9bb7772742f9a79
Iamneo <techadmin@iamneo.ai>
2023-09-23 06:28:43 +0000

    Backup Commit

--- fol/prince.tf
+++ fol/prince.tf
@@ -4,5 +4,22 @@ Secret access key
 AKIA5SE2M7HVZCWITTAW
 	
 TVYVF13/Lg7mvnH6zgIQQ5lmJTqvJcNxBJcZxZEw
-
 */
+
+provider "aws" {
+    region = "ap-south-1"
+    access_key = "AKIA5SE2M7HVZCWITTAW"
+    secret_key = "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"
+}
+
+resource "aws_instance" "prince_instance" {
+    ami = ""
+    instance_type = "t2.micro"
+    tag = {
+        Name = "Prince_Instance"
+    }
+}
+
+output "public_ip" {
+    value = aws_instance.prince_instance.public_ip
+}
